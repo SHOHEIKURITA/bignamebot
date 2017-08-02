f1656b4307fa0979f599de32d6b0b224805cd218
hdymacuser <hdymacuser@MC100167.local>
2017-08-02 11:00:00 +0900

    初めてのGitHub！

---
+++ web/callback_bk2.php
@@ -0,0 +1,54 @@
+<?php
+ 
+$accessToken = getenv('LINE_CHANNEL_ACCESS_TOKEN');
+ 
+//ユーザーからのメッセージ取得
+$json_string = file_get_contents('php://input');
+$json_object = json_decode($json_string);
+ 
+//取得データ
+$replyToken = $json_object->{"events"}[0]->{"replyToken"};        //返信用トークン
+$message_type = $json_object->{"events"}[0]->{"message"}->{"type"};    //メッセージタイプ
+$message_text = $json_object->{"events"}[0]->{"message"}->{"text"};    //メッセージ内容
+ 
+//メッセージタイプが「text」以外のときは何も返さず終了
+if($message_type != "text") exit;
+ 
+//返信メッセージ
+//$return_message_text = "「" . $message_text . "」じゃねーよｗｗｗ";
+
+if ($message_text == '名言') {$return_message_text = "てめぇの人生なんだから。てめぇで走れ。";}
+else {$return_message_text = "やっちゃえNISSAN";}
+ 
+//返信実行
+sending_messages($accessToken, $replyToken, $message_type, $return_message_text);
+?>
+<?php
+//メッセージの送信
+function sending_messages($accessToken, $replyToken, $message_type, $return_message_text){
+    //レスポンスフォーマット
+    $response_format_text = [
+        "type" => $message_type,
+        "text" => $return_message_text
+    ];
+ 
+    //ポストデータ
+    $post_data = [
+        "replyToken" => $replyToken,
+        "messages" => [$response_format_text]
+    ];
+ 
+    //curl実行
+    $ch = curl_init("https://api.line.me/v2/bot/message/reply");
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Content-Type: application/json; charser=UTF-8',
+        'Authorization: Bearer ' . $accessToken
+    ));
+    $result = curl_exec($ch);
+    curl_close($ch);
+}
+?>
